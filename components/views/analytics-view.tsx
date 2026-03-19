18f9a5429569e6f63cc7cd546073e9a4cef108f0
techosystem-hub <s.sukhoroslova@uatechecosystem.com>
2026-03-19 15:20:58 +0100

    Fix: remove unused Card/CardContent import causing build error

--- components/views/analytics-view.tsx
+++ components/views/analytics-view.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState, useEffect, useCallback } from 'react'
-import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
