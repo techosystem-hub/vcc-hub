0268011ba1cce9bcaaae1de43e71f39bf50ac4d3
techosystem-hub <s.sukhoroslova@uatechecosystem.com>
2026-04-08 15:51:07 +0200

    fix: push legend below x-axis labels, height 520px bottom margin 150

--- components/views/deal-room-view.tsx
+++ components/views/deal-room-view.tsx
@@ -316,10 +316,10 @@ function AnalyticsPanel({
             <p className="text-[11px] text-muted-foreground -mt-1">Deals and capital by funding stage</p>
           </CardHeader>
           <CardContent className="flex flex-col flex-1 pt-1 pb-2">
-            <div className="min-h-[440px]">
-            <ResponsiveContainer width="100%" height={440}>
+            <div className="min-h-[520px]">
+            <ResponsiveContainer width="100%" height={520}>
               <ComposedChart data={stats.stagesDist}
-                margin={{ left: 8, right: 8, top: 4, bottom: 110 }}>
+                margin={{ left: 8, right: 8, top: 4, bottom: 150 }}>
                 <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#f0f0f0" />
                 <XAxis type="category" dataKey="name" tick={{ fontSize: 10, fill: '#444', angle: -45, textAnchor: 'end' } as any} interval={0} height={55} />
                 <YAxis type="number" tick={{ fontSize: 10, fill: '#888' }} />
